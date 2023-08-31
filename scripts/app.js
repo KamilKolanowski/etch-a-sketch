6c096705b45f15b45ebb672ea5a4e303166945a9
Kamil Kolanowski <odinstudent@Xubuntu.myguest.virtualbox.org>
2023-08-31 23:01:14 +0200

    feat: add functionality do draw board based on user input

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,22 @@
 const board = document.querySelector(".board");
+const squares = document.getElementsByClassName("square");
 const clearBtn = document.querySelector('.clear-button');
 const submitBtn = document.querySelector('.submit');
 const inputSizeValue = document.querySelector('.input-size');
 
-function generateBoard(size=16) {
-  for (let i = 0; i < size * size; i++) {
-    const newDiv = document.createElement("div");
-    newDiv.classList.add("square");
-    board.appendChild(newDiv);
-
-    newSize = board.offsetWidth / size;
-    newDiv.style.minWidth = `${newSize}px`;
-    newDiv.style.minHeight = `${newSize}px`;
-  }
+function generateBoard(size) {
+    for (let i = 0; i < size * size; i++) {
+      const newDiv = document.createElement("div");
+      newDiv.classList.add("square");
+      board.appendChild(newDiv);
+  
+      newSize = board.offsetWidth / size;
+      newDiv.style.minWidth = `${newSize}px`;
+      newDiv.style.minHeight = `${newSize}px`;
+    }
 }
 
 function changeDivStyle() {
-  const squares = document.getElementsByClassName("square");
   for (let div of squares) {
     div.addEventListener("mouseover", (e) => {
       e.target.style.backgroundColor = "black";
@@ -35,17 +35,25 @@ function clearBoardListener() {
   submitBtn.addEventListener('click', () => clearBoard());
 }
 
-function getUserSize() { 
+function drawUserSizeBoard() { 
   submitBtn.addEventListener('click', () => {
     clearBoardListener();
-    // generateBoard(size)
+    
+    while(board.firstChild) {
+      board.removeChild(board.lastChild);
+    }
+
+    const userValue = inputSizeValue.value;
+    
+    generateBoard(userValue);
+    changeDivStyle();
   });
 }
 
 function etchASketch() {
   clearBoardListener();
   generateBoard(16);
-  getUserSize();
+  drawUserSizeBoard();
   clearBoard();
   changeDivStyle();
 }
